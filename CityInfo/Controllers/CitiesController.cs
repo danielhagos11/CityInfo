5370e96d98ccb8d36c5b643815bfc29918ee3cff
640458 <640458@BJL-GND-06>
2023-10-31 12:21:27 +0000

    Add project files.

---
+++ CityInfo/Controllers/CitiesController.cs
@@ -0,0 +1,32 @@
+﻿using CityInfo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            // find city
+            var cityToReturn = CitiesDataStore.Current.Cities
+                .FirstOrDefault(c => c.Id == id);
+
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+        }
+    }
+}
---
+++ CityInfo/PointOfInterestForCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CityInfo
+{
+    public class PointOfInterestForCreateDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
