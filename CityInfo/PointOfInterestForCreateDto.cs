eb7c8c70f0c76524b3fb3f2d916a857e0d9f9ad1
danielhagos11 <daniehagos@gmail.com>
2023-11-07 09:14:51 +0000

    added crud operations to city info

--- CityInfo/PointOfInterestForCreateDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CityInfo
-{
-    public class PointOfInterestForCreateDto
-    {
-        public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; }
-    }
-}
